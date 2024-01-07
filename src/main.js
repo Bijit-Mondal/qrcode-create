aab35f6c709234076fcd4e2b076ee93cb3fee9bb
Bijit-Mondal <bijitmondal3011@rediffmail.com>
2024-01-07 14:38:57 +0530

    Vuesax Added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import Vuesax from 'vuesax3'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuesax3/dist/vuesax.css' 
+
+const app = createApp(App)
+
+app.use(Vuesax)
+
+app.mount('#app')
